314609b63ab84207a2e30ffd19656524f2293dd0
Nowak20p <k.nowak.p20@gmail.com>
2024-11-14 08:42:37 +0100

    dodanie kontenera

--- Figury/src/components/Figury.jsx
+++ Figury/src/components/Figury.jsx
@@ -53,94 +53,94 @@ function Figury() {
   return (
     <>
       <h1 className="text-center mt-5">Oblicz pole wybranej figury</h1>
-    <div className='m-5'>
-      <select
-        className="form-select mt-5 p-2 mx-auto"
-        onChange={(e) => setFigure(e.target.value)}
-        value={figure}
-      >
-        <option value="">Wybierz figurę</option>
-        <option value="circle">Koło</option>
-        <option value="square">Kwadrat</option>
-        <option value="rectangle">Prostokąt</option>
-        <option value="triangle">Trójkąt</option>
-      </select>
+      <div className="container w-50">
+        <select
+          className="form-select mt-5 p-2 mx-auto"
+          onChange={(e) => setFigure(e.target.value)}
+          value={figure}
+        >
+          <option value="">Wybierz figurę</option>
+          <option value="circle">Koło</option>
+          <option value="square">Kwadrat</option>
+          <option value="rectangle">Prostokąt</option>
+          <option value="triangle">Trójkąt</option>
+        </select>
 
-      {figure === 'circle' && (
-        <div>
-          <input
-            className="form-control mt-2 p-2 mx-auto"
-            type="number"
-            name="radius"
-            placeholder="Promień"
-            value={data.radius}
-            onChange={handleChange}
-          />
-        </div>
-      )}
-      {figure === 'square' && (
-        <div>
-          <input
-            className="form-control mt-2 p-2 mx-auto"
-            type="number"
-            name="side"
-            placeholder="Długość boku"
-            value={data.side}
-            onChange={handleChange}
-          />
-        </div>
-      )}
-      {figure === 'rectangle' && (
-        <div>
-          <input
-            className="form-control mt-2 p-2 mx-auto"
-            type="number"
-            name="base"
-            placeholder="Podstawa"
-            value={data.base}
-            onChange={handleChange}
-          />
-          <input
-            className="form-control mt-2 p-2 mx-auto"
-            type="number"
-            name="height"
-            placeholder="Wysokość"
-            value={data.height}
-            onChange={handleChange}
-          />
-        </div>
-      )}
-      {figure === 'triangle' && (
-        <div>
-          <input
-            className="form-control mt-2 p-2 mx-auto"
-            type="number"
-            name="base"
-            placeholder="Podstawa"
-            value={data.base}
-            onChange={handleChange}
-          />
-          <input
-            className="form-control mt-2 p-2 mx-auto"
-            type="number"
-            name="height"
-            placeholder="Wysokość"
-            value={data.height}
-            onChange={handleChange}
-          />
-        </div>
-      )}
+        {figure === 'circle' && (
+          <div>
+            <input
+              className="form-control mt-2 p-2 mx-auto"
+              type="number"
+              name="radius"
+              placeholder="Promień"
+              value={data.radius}
+              onChange={handleChange}
+            />
+          </div>
+        )}
+        {figure === 'square' && (
+          <div>
+            <input
+              className="form-control mt-2 p-2 mx-auto"
+              type="number"
+              name="side"
+              placeholder="Długość boku"
+              value={data.side}
+              onChange={handleChange}
+            />
+          </div>
+        )}
+        {figure === 'rectangle' && (
+          <div>
+            <input
+              className="form-control mt-2 p-2 mx-auto"
+              type="number"
+              name="base"
+              placeholder="Podstawa"
+              value={data.base}
+              onChange={handleChange}
+            />
+            <input
+              className="form-control mt-2 p-2 mx-auto"
+              type="number"
+              name="height"
+              placeholder="Wysokość"
+              value={data.height}
+              onChange={handleChange}
+            />
+          </div>
+        )}
+        {figure === 'triangle' && (
+          <div>
+            <input
+              className="form-control mt-2 p-2 mx-auto"
+              type="number"
+              name="base"
+              placeholder="Podstawa"
+              value={data.base}
+              onChange={handleChange}
+            />
+            <input
+              className="form-control mt-2 p-2 mx-auto"
+              type="number"
+              name="height"
+              placeholder="Wysokość"
+              value={data.height}
+              onChange={handleChange}
+            />
+          </div>
+        )}
 
-      <button
-        className="btn btn-outline-secondary text-center m-3 p-2 mx-auto w-100"
-        onClick={calculateArea}
-      >
-        Oblicz pole
-      </button>
+        <button
+          className="btn btn-outline-secondary text-center mt-2 p-2 mx-auto w-100"
+          onClick={calculateArea}
+        >
+          Oblicz pole
+        </button>
 
-      <p className="fs-2 mx-auto text-center mt-5 w-100">
-        {result !== null && <div>Pole to: {result.toFixed(2)}cm²</div>}
-      </p>
+        <p className="fs-2 mx-auto text-center mt-5 w-100">
+          {result !== null && <div>Pole to: {result.toFixed(2)}cm²</div>}
+        </p>
       </div>
     </>
   );
